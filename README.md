98a989482653b3c8f6dd97b69e2c9d747e433c3d
Alex <Alexandre LANDREAU>
2020-02-29 13:27:29 +0000

    first try to upload through rpi

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Check solar production with an ATTINY85
+
+
+### Documentation about the ATTINY85
+
+http://ww1.microchip.com/downloads/en/DeviceDoc/Atmel-2586-AVR-8-bit-Microcontroller-ATtiny25-ATtiny45-ATtiny85_Datasheet-Summary.pdf
---
+++ solar-production/solar-production.ino
@@ -0,0 +1,43 @@
+/*
+  Check the tension on the solar pannel and show the value on a display 2 lines (?)
+*/
+
+int solarTensionPin = 1;   // input of the solar tension
+
+// Set pins for screen display
+int sdaPin = 5;
+int sclPin = 7;
+
+// set pins for Communication Module
+int antennaPin = 4;
+
+char message[] ="2 disp";
+
+void setup() {
+  // init analog input
+  pinMode(0, OUTPUT);
+
+  // init screen
+
+  // Send info to Hub
+  //sendMessage("Started Up");
+  displayMessage ("starting up");
+}
+
+void loop() {
+  
+
+  // display a message on a screen
+
+}
+void displayMessage(char info[]){
+	digitalWrite(0, HIGH);  	//Set the LED pins to HIGH. This gives power to the LED and turns it on
+	delay(500);  	// Wait for a half a second
+
+	digitalWrite(0, LOW); 	// Set the LED pins to LOW. This turns it off
+	delay(1000);    // Wait for a half second
+
+}
+void sendMessage(char info[]) {
+
+}
