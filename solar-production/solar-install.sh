bdf7ca1d2b1c884d62220ba22060dcb46aef335c
Alexandre LANDREAU <alexandre@landreau.biz>
2020-03-02 12:21:02 +0000

    change during testing

---
+++ solar-production/solar-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Make solar-production
+MCU="attiny85"
+AVRDUDEMCU="t85"
+AVRDUDE="/opt/avrdude/bin/avrdude"
+AVRDUDECONF="/opt/avrdude/etc/avrdude.conf"
+TARGET="solar-production.cpp"
+
+echo "sudo $AVRDUDE -p $AVRDUDEMCU -C $AVRDUDECONF -c linuxgpio - U flash:w:$TARGET.hex"
+sudo $AVRDUDE -p $AVRDUDEMCU -C $AVRDUDECONF -c linuxgpio - U flash:w:$TARGET.hex
+
